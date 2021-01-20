3ecb79b2bdcaef69f73037d6f54895ec2daeb1c2
Kugge <sofiane.djerbi38@gmail.com>
2021-01-20 15:41:58 +0100

    Added request support

--- server.py
+++ server.py
@@ -1,14 +1,23 @@
 #!/usr/bin/env python
 import flask
+import requests
+
 # Create the application.
 APP = flask.Flask(__name__)
+BLOG_URL = "http://nefault1s.online/Blog.php"
 
 @APP.route('/')
 def index():
     """ Displays the index page accessible at '/'
     """
-    pass
+    return # Empty
 
+@app.route('/news', methods=['POST'])
+def news():
+    print(request.data)
+    print(request.text)
+    #requests.post(BLOG_URL, data={})
+    return data
 
 if __name__ == '__main__':
     APP.debug=True
