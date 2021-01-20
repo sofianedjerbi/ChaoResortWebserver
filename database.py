1e18093ce3a8cf37fe703fdd81a74510a53373bd
Kugge <sofiane.djerbi38@gmail.com>
2021-01-20 22:09:45 +0100

    Fixed PGSQL

--- database.py
+++ database.py
@@ -17,11 +17,14 @@ DATABASE_URL = os.environ['DATABASE_URL']
 BLOG_URL = "http://nefault1s.online/Blog.php"
 conn = psycopg2.connect(DATABASE_URL)
 cur = conn.cursor()
-cur.execute("""CREATE TABLE announcements (
-	id SMALLINT PRIMARY KEY,
-	title VARCHAR (50) UNIQUE NOT NULL,
-	news VARCHAR (512) NOT NULL
- );""")
+try:
+    cur.execute("""CREATE TABLE announcements (
+    	id SMALLINT PRIMARY KEY,
+    	title VARCHAR (50) UNIQUE NOT NULL,
+    	news VARCHAR (512) NOT NULL
+     );""")
+except:
+    pass
 mod_news = {
     11: ("Thanks for downloading this mod![Jan-20-21]", "I hope you'll like it! Chao Resort Island isn't dead yet! We're working on a new character! Click to join the mod discord![https://discord.gg/hycdkQAUKN")
 }
@@ -42,7 +45,7 @@ for i in mod_news:
     news.insert(i, mod_news[i][1])
 
 for i in range(len(titles)):
-    cur.execute(f"INSERT INTO announcements VALUES ({i+1}, \"{titles[i]}\", \"{news[i]}\");")
+    cur.execute(f"INSERT INTO announcements(id, title, news) VALUES ({i+1}, \"{titles[i]}\", \"{news[i]}\");")
 
 con.close()
 # DONE !
