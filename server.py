c7d6bb03a60827a11472089707955c265d97d0b4
Kugge <sofiane.djerbi38@gmail.com>
2021-01-21 02:07:14 +0100

    Fixed syntax

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def index():
 def news():
     over_view = request.form["over_view"]
     get_id = request.form["get_id"]
-    print(f"over:{over_view}; id:{get_id})
+    print(f"over:{over_view}; id:{get_id}")
     if over_view == 1:
         titles = ""
         CUR.execute("SELECT count(*) FROM public.announcements;") # Get cardinal
@@ -36,7 +36,7 @@ def news():
             CUR.execute(f"SELECT * FROM public.announcements WHERE id={i};")
             raw_data = CUR.fetchall()
             titles += raw_data[0][1]
-            print(f"raw:{raw_data}; titles:{titles})
+            print(f"raw:{raw_data}; titles:{titles}")
         return titles # Title list
     else:
         CUR.execute(f"SELECT * FROM public.announcements WHERE id={get_id};")
